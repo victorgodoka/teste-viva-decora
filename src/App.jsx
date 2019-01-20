c8642f081955143f5e0b9c8a1760c2573cfd58df
Pikachu <pikachu@evnts.com.br>
2019-01-20 02:26:35 -0200

    criação de arquivo base de rotas base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import ListaDeFilmes from './Telas/ListaDeFilmes'
 import NaoCurados from './Telas/NaoCurados'
 import styled from 'styled-components'
+import Loading from "./Componentes/Loading";
 
-const Navigation = styled.nav``
+const Navigation = props => <ul>
+  <Item>
+    <Link to="/">Filmes não curados</Link>
+  </Item>
+  <Item>
+    <Link to="/liked">Filmes curtidos</Link>
+  </Item>
+  <Item>
+    <Link to="/desliked">Filmes não curtidos</Link>
+  </Item>
+</ul>
 
 const OpenMenu = styled.ul`
   @media (min-width: 321px) {
@@ -11,7 +23,7 @@ const OpenMenu = styled.ul`
   }
 `
 
-const Menu = styled.menu`
+const MobileMenu = styled.menu`
   & ul {
     padding: 20px;
     height: 100%;
@@ -26,17 +38,72 @@ const Menu = styled.menu`
   }
 `
 
+const Menu = styled.menu`
+  margin-top: 50px;
+  @media (max-width: 320px) {
+    display: none;
+  }
+  & ul {
+    width: 100%;
+    max-width: 720px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    & li {
+      line-height: 1.2;
+      font-size: 1.2rem;
+      color: #fff;
+    }
+  }
+`
+
 const Item = styled.li``
 
-const Imagem = styled.img`
+const MenuIcon = styled.img`
+  @media (min-width: 321px) {
+    display: none;
+  }
+`
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Logo = styled.img`
+  position: absolute;
+  top: -4px;
+  left: calc(50% - 78px);
 `
 
 export default class AppRouter extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      openMenu: false
+      openMenu: false,
+      liked: [],
+      desliked: [],
+      loading: false,
+      currentMovie: 0
     }
+  }
+
+  NaoCuradosRender = ({ match, history }) => {
+    return <NaoCurados updateLoading={this.updateLoading} updateCurrentMovie={this.updateCurrentMovie} currentMovie={this.state.currentMovie} updateMovies={this.updateMovies} history={history} />
+  }
+
+  ListaFilmesRender = ({ match, history }, type) => {
+    return <ListaDeFilmes updateLoading={this.updateLoading} movies={this.state[type]} type={type} history={history} />
+  }
+
+  updateCurrentMovie = currentMovie => {
+    this.setState({ currentMovie })
+  }
+
+  updateLoading = loading => {
+    this.setState({ loading })
   }
 
   toggleMenu = () => {
@@ -49,28 +116,40 @@ export default class AppRouter extends Component {
     })
   }
 
+  updateMovies = (movies, type, timestamp) => {
+    let array = this.state[type]
+    let mov = {
+      id: movies,
+      timestamp
+    }
+    array.push(mov)
+    this.setState({
+      [type]: array
+    })
+  }
+
   render() {
     return <Router>
-      <div className='gradiente'>
-        <header>
-          <Imagem onClick={this.toggleMenu} src='/assets/menu.png' />
-        </header>
+    <div className='filter'>
+      <Loading loading={this.state.loading} />
+        <div className='gradiente'>
+          <Header>
+            <MenuIcon alt='' onClick={this.toggleMenu} src='/assets/menu.png' />
+            <Logo alt='' src='/assets/logo-viva-decora.png' />
+          </Header>
 
-        <Menu>
-          <ul hidden={!this.state.openMenu}>
-            <Item>
-              <Link to="/">Filmes não curados</Link>
-            </Item>
-            <Item>
-              <Link to="/liked">Filmes curtidos</Link>
-            </Item>
-            <Item>
-              <Link to="/desliked">Filmes não curtidos</Link>
-            </Item>
-          </ul>
-        </Menu>
+          <MobileMenu className={'menu'}>
+            <Navigation />
+          </MobileMenu>
 
-        <Route path="/" exact component={NaoCurados} />
+          <Menu>
+            <Navigation />
+          </Menu>
+
+          <Route path="/" exact component={this.NaoCuradosRender} />
+          <Route path="/liked" exact component={props => this.ListaFilmesRender(props, 'liked')} />
+          <Route path="/desliked" exact component={props => this.ListaFilmesRender(props, 'desliked')} />
+        </div>
       </div>
     </Router>
   }
